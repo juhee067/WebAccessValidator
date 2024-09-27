810cf1971c6e13ed9880c5b367319ec933a191f4
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-09-27 17:40:51 +0900

    [chore/#2]:eslint react/react-in-jsx-scope off 추가

---
+++ .eslintrc.cjs
@@ -0,0 +1,121 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/strict',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'import', '@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      'warn',
+      // camelCase 변수, PascalCase 변수, UPPER_CASE 변수 허용
+      {
+        selector: 'variable',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+      },
+      // camelCase 함수, PascalCase 함수 허용
+      {
+        selector: 'function',
+        format: ['camelCase', 'PascalCase'],
+      },
+      // interface 앞에 I 사용 불가
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: false,
+        },
+      },
+      // typeAlias 앞에 T 사용 불가
+      {
+        selector: 'typeAlias',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^T[A-Z]',
+          match: false,
+        },
+      },
+      // typeParameter 앞에 T 사용 불가
+      {
+        selector: 'typeParameter',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^T[A-Z]',
+          match: false,
+        },
+      },
+    ],
+    'no-console': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'react/react-in-jsx-scope': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['builtin', 'external'],
+          'internal',
+          ['parent', 'sibling', 'index'],
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+    'jsx-a11y/alt-text': [
+      'error',
+      {
+        elements: ['img', 'object', 'area', 'input[type="image"]'],
+        img: ['Image'],
+      },
+    ],
+    'jsx-a11y/aria-role': ['error', { ignoreNonDOM: false }],
+    'jsx-a11y/interactive-supports-focus': 'error',
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        assert: 'either',
+      },
+    ],
+    'jsx-a11y/no-noninteractive-element-interactions': [
+      'warn',
+      {
+        handlers: ['onClick', 'onKeyDown', 'onKeyUp', 'onKeyPress'],
+      },
+    ],
+    'jsx-a11y/no-static-element-interactions': [
+      'warn',
+      {
+        handlers: ['onClick', 'onKeyDown', 'onKeyUp', 'onKeyPress'],
+      },
+    ],
+    'jsx-a11y/no-redundant-roles': 'error',
+    'jsx-a11y/interactive-supports-focus': 'error',
+  },
+  overrides: [
+    {
+      files: '**/*.+(ts|tsx)',
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      extends: ['plugin:@typescript-eslint/recommended'],
+    },
+  ],
+};
