231a0e711d537a1e578db54f9f450429b131f843
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-09-29 10:45:32 +0900

    [chore/#2]:sentry 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+import { withSentryConfig } from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: 'web-access-validator',
+  project: 'web-access-validator',
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  reactComponentAnnotation: {
+    enabled: true,
+  },
+  tunnelRoute: '/monitoring',
+  hideSourceMaps: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+});
